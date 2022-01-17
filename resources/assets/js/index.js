f694460dfa30c3f0efd5df35e5dd73393a10df9a
antoine <antoine.guingand@wanadoo.fr>
2022-01-17 16:00:42 +0100

    fix js

--- resources/assets/js/index.js
+++ resources/assets/js/index.js
@@ -1,3 +1,18 @@
 import 'vanilla-cookieconsent';
 
 window.cookieconsent = initCookieConsent();
+
+const config = window.cookieConsentConfig;
+
+cookieconsent.run({
+    ...config,
+    languages: Object.values(config.languages).map(lang => {
+        lang.settings_modal.blocks.forEach(block => {
+            block.cookie_table = block.cookie_table?.map(row => ({
+                ...row,
+                col2: row.col2 ?? location.host,
+            }));
+        });
+        return lang;
+    }),
+});
