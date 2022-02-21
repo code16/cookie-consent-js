dbd3f52f20ee01eddd2335f34f8063f60230baf0
antoine <antoine.guingand@wanadoo.fr>
2022-02-21 11:23:25 +0100

    fix localized

--- resources/assets/js/index.js
+++ resources/assets/js/index.js
@@ -8,16 +8,18 @@ cookieconsent.showSettings = delay => {
     return false;
 };
 
-const config = window.cookieConsentConfig;
-cookieconsent.run({
-    ...config,
-    languages: Object.values(config.languages).map(lang => {
+cookieconsent.run(getConfig());
+
+
+function getConfig() {
+    const config = window.cookieConsentConfig;
+    Object.values(config.languages).forEach(lang => {
         lang.settings_modal.blocks.forEach(block => {
             block.cookie_table = block.cookie_table?.map(row => ({
                 ...row,
                 col2: row.col2 ?? location.hostname,
             }));
         });
-        return lang;
-    }),
-});
+    });
+    return config;
+}
